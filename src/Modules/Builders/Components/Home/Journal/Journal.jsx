37df3439cfcc6e5d21adbf1b52fd60ed49b9309b
Vu Nguyen <nguyenthevu297@gmail.com>
2023-01-03 16:34:21 +0700

    feat: Update journal section

--- src/Modules/Builders/Components/Home/Journal/Journal.jsx
+++ src/Modules/Builders/Components/Home/Journal/Journal.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Journal.css"
 
 function Journal() {
     return (
@@ -7,16 +8,59 @@ function Journal() {
                 <div className="orebi-journal__inner">
                     <div className="orebi-journal__items">
                         <div className="orebi-brands__center-title">
-                            <h2 className='black'></h2>
+                            <h2 className='h2--black'>Journal</h2>
                         </div>
                         <div className="orebi-brands__center-content">
-                            <div className="content__image">
-                                <img src="" alt="" />
+                            <div className="orebi-brands__center-content__wrapper">
+                                <div className="orebi-brands__center-content__wrapper-image">
+                                    <img src="/src/assets/d07f00c7f8bf20e179ae.jpg" alt="" />
+                                </div>
+                                <div className="orebi-brands__center-content__wrapper-item">
+                                    <div className="orebi-brands__center-content__wrapper-item__text">
+                                        <p>Category</p>
+                                    </div>
+                                    <div className="orebi-brands__center-content__wrapper-item__title">
+                                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
+                                    </div>
+                                    <div className="orebi-brands__center-content__wrapper-item__des">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
-                            <div className="content-item">
-                                <div className="content-item__text"></div>
-                                <div className="content-item__title"></div>
-                                <div className="content-item__des"></div>
+                            <div className="orebi-brands__center-content__wrapper">
+                                <div className="orebi-brands__center-content__wrapper-image">
+                                    <img src="/src/assets/d07f00c7f8bf20e179ae.jpg" alt="" />
+                                </div>
+                                <div className="orebi-brands__center-content__wrapper-item">
+                                    <div className="orebi-brands__center-content__wrapper-item__text">
+                                        <p>Category</p>
+                                    </div>
+                                    <div className="orebi-brands__center-content__wrapper-item__title">
+                                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
+                                    </div>
+                                    <div className="orebi-brands__center-content__wrapper-item__des">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="orebi-brands__center-content__wrapper">
+                                <div className="orebi-brands__center-content__wrapper-image">
+                                    <img src="/src/assets/d07f00c7f8bf20e179ae.jpg" alt="" />
+                                </div>
+                                <div className="orebi-brands__center-content__wrapper-item">
+                                    <div className="orebi-brands__center-content__wrapper-item__text">
+                                        <p>Category</p>
+                                    </div>
+                                    <div className="orebi-brands__center-content__wrapper-item__title">
+                                        <h4>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h4>
+                                    </div>
+                                    <div className="orebi-brands__center-content__wrapper-item__des">
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
